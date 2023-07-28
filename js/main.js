a449c00317a908d1a3e3157a4aaa49a4734ffcb4
CoraBJohnson <111707632+CoraBJohnson@users.noreply.github.com>
2023-07-27 21:10:56 -0500

    update layout and basic map element

---
+++ js/main.js
@@ -0,0 +1,18 @@
+//initialize Leaflet
+var map = L.map('map').setView([39.59, -85.23], 16);
+
+//add tile layer
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+    attribution: ' &copy; Esri'
+}).addTo(map);
+
+//add reference tile layer
+L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: 19,
+}).addTo(map);
+
+// //add the zoom control
+// L.control.zoom({
+//     position: 'bottomleft'
+// }).addTo(map);
