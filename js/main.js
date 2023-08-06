30f17de17e1407f54e42d83df8b0cc26c697695e
CoraBJohnson <111707632+CoraBJohnson@users.noreply.github.com>
2023-08-06 16:42:36 -0500

    configure esri map

--- js/main.js
+++ js/main.js
@@ -1,46 +1,47 @@
-//Create Leaflet map
-//function to create map with tile layer
-function createMap() {
-    var map = L.map('map', {
-        center: [39.59, -85.23],
-        zoom: 16,
-        maxZoom:19
-        boxZoom: false,
-        doubleClickZoom: false,
-        keyboard: false,
-        zoomControl: true
-    });
-    //add tile layer
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        attribution: '&copy; Esri'
-    }).addTo(map);
-
-    //add reference tile layer
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
-    }).addTo(map);
-
-    //call getData function within the create map function to add data to map
-    //getData(map);
-
-};
-
-// function createMap(){
-//     //create the map
-    var map = L.map('map').setView([39.59, -85.23], 16);
-
-    //add tile layer
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        maxZoom: 19,
-        attribution: ' &copy; Esri'
-    }).addTo(map);
-
-    //add reference tile layer
-    L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
-        maxZoom: 19,
-    }).addTo(map);
-
-
-$(document).ready(createMap);
+// //Create Leaflet map
+// //function to create map with tile layer
+// function createMap() {
+//     var map = L.map('map', {
+//         center: [39.59, -85.23],
+//         zoom: 16,
+//         maxZoom:19
+//         boxZoom: false,
+//         doubleClickZoom: false,
+//         keyboard: false,
+//         zoomControl: true
+//     });
+//     //add tile layer
+//     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+//         attribution: '&copy; Esri'
+//     }).addTo(map);
+//
+//     //add reference tile layer
+//     //add reference tile layer
+//     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
+//     }).addTo(map);
+//
+//     //call getData function within the create map function to add data to map
+//     //getData(map);
+//
+// };
+//
+// // function createMap(){
+// //     //create the map
+//     var map = L.map('map').setView([39.59, -85.23], 16);
+//
+//     //add tile layer
+//     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+//         maxZoom: 19,
+//         attribution: ' &copy; Esri'
+//     }).addTo(map);
+//
+//     //add reference tile layer
+//     L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Boundaries_and_Places/MapServer/tile/{z}/{y}/{x}', {
+//         maxZoom: 19,
+//     }).addTo(map);
+//
+//
+// $(document).ready(createMap);
 
 // // function for original symbology with popups
 // function symbolize(data, map){
